a19095012c75d4f692f26c8219b5983a9e6f7479
Mahmud <mahmudur69@student.sust.edu>
2022-06-04 12:23:10 +0600

    initial game environment setup

---
+++ circle-jumper/core/src/com/jga/jumper/config/GameConfig.java
@@ -0,0 +1,18 @@
+package com.jga.jumper.config;
+
+public class GameConfig {
+
+    public static final float WIDTH = 600f; //pixels
+    public static final float HEIGHT = 800f;
+
+    public static final float WORLD_WIDTH = 16f; //world units
+    public static final float WORLD_HEIGHT = 24f;
+
+    public static final float WORLD_CENTER_X = WORLD_WIDTH / 2f;
+    public static final float WORLD_CENTER_Y = WORLD_HEIGHT / 2f;
+
+
+    private GameConfig(){
+
+    };
+}
---
+++ circle-jumper/core/src/com/jga/jumper/screen/game/GameScreen.java
@@ -0,0 +1,16 @@
+package com.jga.jumper.screen.game;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.assets.AssetManager;
+import com.jga.jumper.CircleJumperGame;
+
+public class GameScreen extends ScreenAdapter {
+
+    private final CircleJumperGame game;
+    private final AssetManager assetManager;
+
+    public GameScreen(CircleJumperGame game){
+        this.game = game;
+        assetManager = game.getAssetManager();
+    }
+}
